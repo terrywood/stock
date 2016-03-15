af66fc03d13b6c82997de607ea3e68b0ed4bf0fa
terry.wu <terry.wu@gtomato.com>
2016-03-15 14:38:56 +0800

    股东人数

--- src/test/java/TestMain.java
+++ src/test/java/TestMain.java
@@ -8,6 +8,8 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+
+import java.net.URL;
 import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -17,8 +19,8 @@ import java.util.regex.Pattern;
  */
 public class TestMain {
     public static void main(String[] args) throws Exception {
-     //  String str ="截至2016年2月15日，公司股东总数为154,857。多谢您对公司的关注！" ;
-      // Pattern pattern = Pattern.compile("([0-9]+[,]?)+");Matcher matcher = pattern.matcher(str);
+        //  String str ="截至2016年2月15日，公司股东总数为154,857。多谢您对公司的关注！" ;
+        // Pattern pattern = Pattern.compile("([0-9]+[,]?)+");Matcher matcher = pattern.matcher(str);
         //List<Integer> list = new ArrayList<Integer>();
  /*       int renCount=0;
         while(matcher.find()){
@@ -31,8 +33,37 @@ public class TestMain {
         }
 
 */
-        Map<String,String> map = new HashMap<String,String>();
-        map.put("Status","0");
-        System.out.println( JSONUtils.toJSONString(map));
+       /* Map<String, String> map = new HashMap<String, String>();
+        map.put("Status", "0");
+        System.out.println(JSONUtils.toJSONString(map));*/
+
+        Document doc = Jsoup.connect("http://www.djstg.com/stock/000100/xml/gdyj/gdhs.xml")
+                .userAgent("Mozilla")
+                .timeout(3000)
+                .get();
+
+
+        Elements elements = doc.getElementsByTag("dataset");
+        Elements set =  elements.get(0).getElementsByTag("set");
+        for(int i=0;i<set.size();i++){
+            Element ele  =set.get(i);
+            System.out.println(ele.attr("value"));
+            System.out.println(ele.attr("tooltext"));
+        }
+
+
+       // System.out.println(elements.get(0).html());
+        //System.out.println(elements.get(1).html());
+      /*  String link = "http://www.djstg.com/stock/000100/xml/gdyj/gdhs.xml";
+        SAXReader reader = new SAXReader();
+        Document doc = reader.read(new URL(link));
+        Element root = doc.getRootElement();
+        Element foo;
+        Iterator iterator  =root.elementIterator("dataset");
+        while (iterator.hasNext()){
+            foo = (Element) iterator.next();
+            System.out.print("车牌号码:" + foo.asXML());
+        }*/
+
     }
 }
